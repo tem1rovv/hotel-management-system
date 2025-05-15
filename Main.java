a0c66c0437a1d529171d1f6a2fe1ec2b233ab20e
tem1rovv <abbostemirov12321@gmail.com>
2025-05-15 09:41:41 +0500

    Main.java add context

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import project.service.AuthService;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("-----hotel management system-----");
         AuthService authService =new AuthService();
         authService.service();
     }
